ecfaa73008180345c48324994fa16297455d770c
Mark Kanko <mkanko66@gmail.com>
2019-03-29 21:51:41 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -48,10 +48,11 @@ end
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   holiday_hash[season][holiday_name] = supply_array
   holiday_hash
+end
   # code here
   # remember to return the updated hash
 
-end
+
 
 def all_winter_holiday_supplies(holiday_hash)
   # winter_supplies = []
